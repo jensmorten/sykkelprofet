b374de58a38430d0dc6674ce7042d8743e9e6c0f
jensmorten <jens.morten.nilsen@gmail.com>
2026-02-13 09:24:44 +0100

    Update app.py

--- konkurranse/app.py
+++ konkurranse/app.py
@@ -140,4 +140,4 @@ if uploaded_file and team_name:
             leaderboard = leaderboard.sort_values("rmse").reset_index(drop=True)
             leaderboard.to_csv(LEADERBOARD_FILE, index=False)
 
-            st.st.rerun()
+            st.rerun()
