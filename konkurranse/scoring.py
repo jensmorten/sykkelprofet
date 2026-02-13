4a9ae37222c09dadf4f4409cc21c65c787c2ad22
jensmorten <jens.morten.nilsen@gmail.com>
2026-02-13 08:25:05 +0100

    work

--- konkurranse/scoring.py
+++ konkurranse/scoring.py
@@ -7,7 +7,7 @@ from sklearn.metrics import root_mean_squared_error
 # --------------------------------------------------
 
 TARGET_FILE = "test_target_secret.csv"
-SUBMISSION_FILE = "submission_testing_w.csv"
+SUBMISSION_FILE = "submission_testing_a.csv"
 
 # PNG lagres med samme navn som submission
 OUTPUT_PLOT = SUBMISSION_FILE.replace(".csv", "_score.png")
