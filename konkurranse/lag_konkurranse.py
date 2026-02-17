42b6466f823522b0ccdde18146b1d26e0f18a017
jensmorten <jens.morten.nilsen@gmail.com>
2026-02-17 14:49:37 +0100

    work

--- konkurranse/lag_konkurranse.py
+++ konkurranse/lag_konkurranse.py
@@ -2,13 +2,15 @@ import pandas as pd
 
 # Les original testsett
 df_test = pd.read_csv("bysykkel_test.csv", parse_dates=["ds"])
+print(df_test.count())
 
-df_target = df_test[["ds", "y"]]
+df_target = df_test[["ds", "y"]].copy()
+print(df_target.count())
 df_target.to_csv("test_target_secret.csv", index=False)
 
 # Fjern y
 df_public = df_test.drop(columns=["y"])
-
+print(df_public.count())
 df_public.to_csv("test_compete.csv", index=False)
 
 print("✅ Laget test_compete.csv (uten y)")
