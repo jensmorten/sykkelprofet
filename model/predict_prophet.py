21131ac49c5b189835d7066d63ceece9f92a6b29
jensmorten <jens.morten.nilsen@gmail.com>
2026-03-19 13:58:26 +0100

    wrk

--- model/predict_prophet.py
+++ model/predict_prophet.py
@@ -45,50 +45,70 @@ regressors = [
 ]
 
 future = pred[["ds"] + regressors].copy()
+
 forecast = m.predict(future)
 
 predictions = pd.DataFrame({
     "ds": pred["ds"],
+
+    # prediksjon
     "yhat": forecast["yhat"],
+    "yhat_log": forecast["yhat"],
+
+    # komponentar
+    "trend": forecast["trend"],
+    "weekly": forecast.get("weekly", 0),
+    "daily": forecast.get("daily", 0),
+    "yearly": forecast.get("yearly", 0),
+
+    # vær
     "temperatur (C)" : pred['air_temperature'],
     "effektiv_temperatur (C)": pred['feels_like'],
     "nedbør (mm)" : pred['precipitation_amount'],
     "vind (m/s)" : pred['wind_speed']
 })
 
+predictions["seasonality"] = (
+    predictions["weekly"]
+    + predictions["daily"]
+    + predictions["yearly"]
+)
 
-predictions["yhat"] = np.expm1(predictions["yhat"])  # exp(yhat) - 1
+# transform yhat til faktisk nivå
+predictions["yhat"] = np.expm1(predictions["yhat"])
 predictions["yhat"] = predictions["yhat"].clip(lower=0)
 predictions["yhat"] = predictions["yhat"].round().astype(int)
 
-print(predictions)
+# baseline (trend)
+predictions["effect_trend"] = np.expm1(predictions["trend"])
 
-predictions.to_csv(f"predictions.csv", index=False, sep=',')
+# trend + sesong
+trend_plus_season = predictions["trend"] + predictions["seasonality"]
 
-###same dates from last year
-history = pd.read_csv("https://raw.githubusercontent.com/jensmorten/sykkelprofet/refs/heads/main/data/bysykkel_history2018-2025.csv", parse_dates=["ds"])
-
-start=predictions['ds'].min().date()+ timedelta(weeks=-52)
-end = predictions['ds'].max().date()+ timedelta(weeks=-52)
-
-#print(start)
-#print(end)
-
-history = history[history['ds'].dt.date >start]
-history = history[history['ds'].dt.date <end]
-
-
-history["feels_like"] = f.compute_feels_like(
-    history["air_temperature"].values,
-    history["wind_speed"].values
+# sesong-effekt (delta frå trend)
+predictions["effect_seasonality"] = (
+    np.expm1(trend_plus_season)
+    - np.expm1(predictions["trend"])
 )
 
-history = pd.DataFrame({
-    "ds": history["ds"],
-    "y": history["y"],
-    "effektiv_temperatur (C)": history['feels_like'],
-    "nedbør (mm)" : history['precipitation_amount'],
-    "vind (m/s)" : history['wind_speed']
-})
+# vær-effekt (delta frå trend + sesong)
+predictions["effect_weather"] = (
+    np.expm1(predictions["yhat_log"])
+    - np.expm1(trend_plus_season)
+)
 
-#print(history)
+predictions["effect_trend"] = predictions["effect_trend"].round().astype(int)
+predictions["effect_seasonality"] = predictions["effect_seasonality"].round().astype(int)
+predictions["effect_weather"] = predictions["effect_weather"].round().astype(int)
+
+# la vær bli restledd (slik at det alltid stemmer)
+predictions["effect_weather"] = (
+    predictions["yhat"]
+    - predictions["effect_trend"]
+    - predictions["effect_seasonality"]
+)
+
+
+print(predictions.head(5))
+
+predictions.to_csv(f"predictions.csv", index=False, sep=',')
