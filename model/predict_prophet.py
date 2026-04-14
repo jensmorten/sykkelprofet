fbcfea4cdd7ab10378181cf3130939c27a5c5289
jensmorten <jens.morten.nilsen@gmail.com>
2026-04-14 18:20:15 +0200

    Update predict_prophet.py

--- model/predict_prophet.py
+++ model/predict_prophet.py
@@ -114,4 +114,4 @@ predictions["effect_weather"] = (
 
 print(predictions)
 
-predictions.to_csv(f"predictions.csv", index=False, sep=',')
+predictions.to_csv("model/predictions.csv", index=False, sep=',')
