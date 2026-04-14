1e9331b4f5746b1a4b08683a1ea11babff27aada
jensmorten <jens.morten.nilsen@gmail.com>
2026-04-14 17:56:03 +0200

    Update predict_prophet.py

--- model/predict_prophet.py
+++ model/predict_prophet.py
@@ -7,11 +7,10 @@ from datetime import datetime, timedelta
 
 print("modell og data for prediksjon...")
 with open('model/prophet_model.json', 'r') as fin:
-#with open('https://raw.githubusercontent.com/jensmorten/sykkelprofet/refs/heads/main/model/prophet_model.json', 'r') as fin:
     m = model_from_json(json.load(fin))
 
-pred = pd.read_csv("https://raw.githubusercontent.com/jensmorten/sykkelprofet/refs/heads/main/model/current_weather_forecast.csv", parse_dates=["ds"])
-#pred=pd.read_csv("current_weather_forecast.csv", parse_dates=["ds"])
+#pred = pd.read_csv("https://raw.githubusercontent.com/jensmorten/sykkelprofet/refs/heads/main/model/current_weather_forecast.csv", parse_dates=["ds"])
+pred=pd.read_csv("model/current_weather_forecast.csv", parse_dates=["ds"])
 pred["ds"] = pred["ds"].dt.tz_localize(None)
 
 pred = pred.sort_values("ds")
